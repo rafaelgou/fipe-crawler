38ba884c47fa0b8f15a3a0283d5595a591ce5c9b
Rafael Goulart <rafaelgou@gmail.com>
2017-01-31 15:27:34 -0200

    Ajusta requisição XHR

--- src/Fipe/Crawler.php
+++ src/Fipe/Crawler.php
@@ -164,6 +164,19 @@ class Crawler {
         curl_setopt($ch,CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, count($postData));
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        		'Accept:application/json, text/javascript, */*; q=0.01',
+        		'Accept-Encoding:gzip, deflate',
+        		'Accept-Language:pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4',
+        		'Connection:keep-alive',
+        		'Content-Type:application/x-www-form-urlencoded; charset=UTF-8',
+        		'Cookie:_ga=GA1.3.472052299.1466616166; _gat=1',
+        		'Host:veiculos.fipe.org.br',
+        		'Origin:http://veiculos.fipe.org.br',
+        		'Referer:http://veiculos.fipe.org.br/',
+        		'User-Agent:Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+        		'X-Requested-With:XMLHttpRequest'
+          ));
 
         $output=curl_exec($ch);
         curl_close($ch);
